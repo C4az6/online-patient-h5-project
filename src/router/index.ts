ea776e9ee8694bc6392001a62feb29980bd2c6fe
c4az6 <1762458611@qq.com>
2023-04-13 22:09:20 +0800

    登录路由与组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,6 +21,11 @@ const router = createRouter({
       path: '/test',
       name: 'test',
       component: Test
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login/index.vue')
     }
   ]
 })
